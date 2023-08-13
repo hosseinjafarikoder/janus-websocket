6cca8697b38383ebb0e690edb364a6836c4bc810
hossein <ho.jafari@dotin.ir>
2023-08-13 20:48:44 +0330

    start project

---
+++ JanusWebsocket/src/main/java/videocall/janus/januswebsocket/JanusWebsocketApplication.java
@@ -0,0 +1,13 @@
+package videocall.janus.januswebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanusWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JanusWebsocketApplication.class, args);
+    }
+
+}
---
+++ JanusWebsocket/src/test/java/videocall/janus/januswebsocket/JanusWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package videocall.janus.januswebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanusWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
